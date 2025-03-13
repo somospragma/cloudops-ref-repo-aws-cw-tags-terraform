fba63580e205939dadb66549a35e1bcba9a2c9b5
farciniegas52 <felipe.arciniegas@pragma.com.co>
2025-03-13 15:54:28 -0500

    Update Module

--- sample/main.tf
+++ sample/main.tf
@@ -1,18 +1,18 @@
 module "observability" {
-  source = "./module/observability"
+  source = "../"
 
-  client        = var.client
-  project       = var.project
-  environment   = var.environment
-  application   = var.application
+  client      = var.client
+  project     = var.project
+  environment = var.environment
+  application = var.application
 
   ec2 = {
     functionality    = "ec2"
-    create_dashboard = true
-    create_alarms    = true
     tag_key          = "EnableObservability"
     tag_value        = "true"
+    create_dashboard = true
     metrics          = ["CPUUtilization", "NetworkIn", "NetworkOut"]
+    create_alarms    = true
     alarm_thresholds = {
       "CPUUtilization" = 75,
       "NetworkIn"      = 60000000,
@@ -20,32 +20,31 @@ module "observability" {
     }
   }
 
-  rds = {
-    functionality    = "rds"
-    create_dashboard = true
-    create_alarms    = true
-    tag_key          = "EnableObservability"
-    tag_value        = "true"
-    metrics          = ["CPUUtilization", "DatabaseConnections", "FreeableMemory"]
-    alarm_thresholds = {
-      "CPUUtilization"      = 75,
-      "DatabaseConnections" = 80,
-      "FreeableMemory"      = 1500000000
-    }
-  }
-
   lambda = {
-    functionality    = "lambda"
+    functionality = "lambda"
+    tag_key       = "EnableObservability"
+    tag_value     = "true"
     create_dashboard = true
-    create_alarms    = true
-    tag_key         = "EnableObservability"
-    tag_value       = "true"
-    metrics         = ["Invocations", "Duration", "Errors", "Throttles"]
+    metrics         = ["Invocations", "Duration", "Errors"]
+    create_alarms = true
     alarm_thresholds = {
       "Invocations" = 1000
       "Duration"    = 2000
       "Errors"      = 5
-      "Throttles"   = 10
+    }
+  }
+
+  rds = {
+    functionality    = "rds"
+    tag_key          = "EnableObservability"
+    tag_value        = "true"
+    create_dashboard = true
+    metrics          = ["CPUUtilization", "DatabaseConnections", "FreeableMemory"]
+    create_alarms    = true
+    alarm_thresholds = {
+      "CPUUtilization"      = 75,
+      "DatabaseConnections" = 80,
+      "FreeableMemory"      = 1500000000
     }
   }
 
@@ -56,10 +55,10 @@ module "observability" {
     tag_value        = "true"
     metrics          = ["RequestCount", "TargetResponseTime", "HTTPCode_ELB_5XX_Count", "4xx_count"]
     alarm_thresholds = {
-      RequestCount               = 50000
-      TargetResponseTime         = 2
-      HTTPCode_ELB_5XX_Count     = 5
-      "4xx_count"                = 3
+      RequestCount           = 50000
+      TargetResponseTime     = 2
+      HTTPCode_ELB_5XX_Count = 5
+      "4xx_count"            = 3
     }
   }
 
@@ -97,27 +96,26 @@ module "observability" {
     tag_value        = "true"
     metrics          = ["Latency", "5XXError", "4XXError", "IntegrationLatency"]
     alarm_thresholds = {
-      "Latency"           = 500
-      "5XXError"          = 5
-      "4XXError"          = 10
+      "Latency"            = 500
+      "5XXError"           = 5
+      "4XXError"           = 10
       "IntegrationLatency" = 1000
     }
   }
 
-  dynamodb = {
-    functionality    = "dynamodb"
-    create_dashboard = true
-    create_alarms    = true
-    tag_key          = "EnableObservability"
-    tag_value        = "true"
-    metrics          = ["ConsumedReadCapacityUnits", "ConsumedWriteCapacityUnits", "ThrottledRequests", "ReadThrottleEvents", "WriteThrottleEvents"]
-    alarm_thresholds = {
-      "ConsumedReadCapacityUnits"  = 1000
-      "ConsumedWriteCapacityUnits" = 500
-      "ThrottledRequests"          = 5
-      "ReadThrottleEvents"         = 3
-      "WriteThrottleEvents"        = 3
-    }
-  }
-
+  # dynamodb = {
+  #   functionality    = "dynamodb"
+  #   create_dashboard = true
+  #   create_alarms    = true
+  #   tag_key          = "EnableObservability"
+  #   tag_value        = "true"
+  #   metrics          = ["ConsumedReadCapacityUnits", "ConsumedWriteCapacityUnits", "ThrottledRequests", "ReadThrottleEvents", "WriteThrottleEvents"]
+  #   alarm_thresholds = {
+  #     "ConsumedReadCapacityUnits"  = 1000
+  #     "ConsumedWriteCapacityUnits" = 500
+  #     "ThrottledRequests"          = 5
+  #     "ReadThrottleEvents"         = 3
+  #     "WriteThrottleEvents"        = 3
+  #   }
+  # }
 }
